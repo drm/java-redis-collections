1f8eeed607e74f229dea5c99bbd06456e58b5526
Gerard van Helden <gerard@van-helden.name>
2020-05-08 16:50:56 +0200

    add cleanup checks

--- test/nl/melp/redis/collections/IntegrationTest.java
+++ test/nl/melp/redis/collections/IntegrationTest.java
@@ -1,9 +1,9 @@
 package nl.melp.redis.collections;
 
 import nl.melp.redis.Redis;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -16,273 +16,248 @@ import static org.junit.Assert.*;
 
 public class IntegrationTest {
 	private final String keyName = IntegrationTest.class.getCanonicalName();
-	private static Redis redis;
+	private Redis redis;
+	private Socket socket;
 
-	@BeforeClass
-	public static void init() {
-		redis =
+	@Before
+	public void init() throws IOException {
+		socket = new Socket("localhost", 6379);
+		redis = new Redis(socket);
+		redis.call("SELECT", "15");
+		Assert.assertEquals("Refusing to run on non-empty database", 0, ((List<?>) redis.call("KEYS", "*")).size());
 	}
 
-	private void clear() {
-		try {
-			Redis.run((redis) -> redis.call("DEL", keyName), "localhost", 6379);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
+	@After
+	public void cleanup() throws IOException {
+		int size = ((List<?>) redis.call("KEYS", "*")).size();
+		redis.call("QUIT");
+		if (!socket.isClosed()) {
+			socket.close();
+		}
+		if (size > 0) {
+			Assert.fail("This test is littering");
 		}
 	}
+
 
 	@Test
 	public void testList() throws IOException {
-		clear();
-		try (Socket socket = new Socket("localhost", 6379)) {
-			ByteArrayList s = new ByteArrayList(new Redis(socket), keyName);
+		ByteArrayList s = new ByteArrayList(redis, keyName);
 
-			s.push("A".getBytes());
-			s.push("B".getBytes());
-			s.push("C".getBytes());
+		s.push("A".getBytes());
+		s.push("B".getBytes());
+		s.push("C".getBytes());
 
-			assertEquals(3, s.size());
-			assertEquals("C", new String(s.pop()));
-			assertEquals("B", new String(s.pop()));
-			assertEquals("A", new String(s.pop()));
+		assertEquals(3, s.size());
+		assertEquals("C", new String(s.pop()));
+		assertEquals("B", new String(s.pop()));
+		assertEquals("A", new String(s.pop()));
 
-			assertEquals(0, s.size());
-			assertTrue(s.isEmpty());
+		assertEquals(0, s.size());
+		assertTrue(s.isEmpty());
 
-			s.push("A".getBytes());
-			s.push("B".getBytes());
-			assertEquals("A", new String(s.get(0)));
+		s.push("A".getBytes());
+		s.push("B".getBytes());
+		assertEquals("A", new String(s.get(0)));
 
-			s.clear();
-			assertEquals(0, s.size());
-			assertTrue(s.isEmpty());
-		}
+		s.clear();
+		assertEquals(0, s.size());
+		assertTrue(s.isEmpty());
 	}
 
 	@Test
 	public void testMap() throws IOException {
-		clear();
-
-		try (Socket socket = new Socket("localhost", 6379)) {
-			Map<byte[], byte[]> t = new ByteArrayMap(new Redis(socket), keyName);
-			assertEquals(0, t.size());
-			t.put("Key".getBytes(), "Value".getBytes());
-			assertEquals(1, t.size());
-			assertTrue(t.containsKey("Key".getBytes()));
-			assertEquals("Value", new String(t.get("Key".getBytes())));
-			t.clear();
-			assertEquals(0, t.size());
-		}
-
-		clear();
+		Map<byte[], byte[]> t = new ByteArrayMap(redis, keyName);
+		assertEquals(0, t.size());
+		t.put("Key".getBytes(), "Value".getBytes());
+		assertEquals(1, t.size());
+		assertTrue(t.containsKey("Key".getBytes()));
+		assertEquals("Value", new String(t.get("Key".getBytes())));
+		t.clear();
+		assertEquals(0, t.size());
 	}
 
 	@Test
 	public void testSerializedMap() throws IOException {
-		clear();
-
-		try (Socket socket = new Socket("localhost", 6379)) {
-			Map<String, String> t = new SerializedHashMap<>(new Redis(socket), keyName);
-			assertEquals(0, t.size());
-			t.put("Key", "Value");
-			assertEquals(1, t.size());
-			assertTrue(t.containsKey("Key"));
-			assertEquals("Value", t.get("Key"));
-			t.clear();
-			assertEquals(0, t.size());
-		}
+		Map<String, String> t = new SerializedHashMap<>(redis, keyName);
+		assertEquals(0, t.size());
+		t.put("Key", "Value");
+		assertEquals(1, t.size());
+		assertTrue(t.containsKey("Key"));
+		assertEquals("Value", t.get("Key"));
+		t.clear();
+		assertEquals(0, t.size());
 	}
 
 	@Test
 	public void testSet() throws Exception {
-		clear();
+		ByteArraySet t = new ByteArraySet(redis, keyName);
 
-		try (Socket socket = new Socket("localhost", 6379)) {
-			ByteArraySet t = new ByteArraySet(new Redis(socket), keyName);
+		Iterator<byte[]> iterator = t.iterator();
+		assertFalse(iterator.hasNext());
+		assertFalse(iterator.hasNext());
+		assertNull(iterator.next());
 
-			Iterator<byte[]> iterator = t.iterator();
-			assertFalse(iterator.hasNext());
-			assertFalse(iterator.hasNext());
-			assertNull(iterator.next());
-
-			// test empty set iteration
-			for (byte[] b : t) {
-				throw new Exception("Should be empty");
-			}
-
-			assertEquals(0, t.size());
-			t.add("Hello".getBytes());
-			assertEquals(1, t.size());
-
-			// test single item iteration
-			for (byte[] b : t) {
-				assertEquals(new String(b), "Hello");
-			}
-			t.clear();
-
-			assertEquals(0, t.size());
-			t.add("Val1".getBytes());
-			t.add("Val2".getBytes());
-			t.add("Val2".getBytes());
-			assertEquals(2, t.size());
-			assertTrue(t.contains("Val1".getBytes()));
-			assertTrue(t.contains("Val2".getBytes()));
-			assertTrue(!t.contains("Val3".getBytes()));
-			assertTrue(t.remove("Val1".getBytes()));
-			assertTrue(!t.remove("Val1".getBytes()));
-			t.clear();
-			assertEquals(0, t.size());
-
-			Set<String> s = new HashSet<>();
-			for (int i = 0; i < 1000; i ++) {
-				String str = Integer.toString(i);
-				s.add(str);
-				t.add(str.getBytes());
-			}
-
-			List<String> l = new LinkedList<>();
-			for (byte[] b : t) {
-				l.add(new String(b));
-			}
-			assertTrue(s.containsAll(l));
-			assertEquals(s.size(), l.size());
+		// test empty set iteration
+		for (byte[] b : t) {
+			throw new Exception("Should be empty");
 		}
 
-		clear();
+		assertEquals(0, t.size());
+		t.add("Hello".getBytes());
+		assertEquals(1, t.size());
+
+		// test single item iteration
+		for (byte[] b : t) {
+			assertEquals(new String(b), "Hello");
+		}
+		t.clear();
+
+		assertEquals(0, t.size());
+		t.add("Val1".getBytes());
+		t.add("Val2".getBytes());
+		t.add("Val2".getBytes());
+		assertEquals(2, t.size());
+		assertTrue(t.contains("Val1".getBytes()));
+		assertTrue(t.contains("Val2".getBytes()));
+		assertTrue(!t.contains("Val3".getBytes()));
+		assertTrue(t.remove("Val1".getBytes()));
+		assertTrue(!t.remove("Val1".getBytes()));
+		t.clear();
+		assertEquals(0, t.size());
+
+		Set<String> s = new HashSet<>();
+		for (int i = 0; i < 1000; i++) {
+			String str = Integer.toString(i);
+			s.add(str);
+			t.add(str.getBytes());
+		}
+
+		List<String> l = new LinkedList<>();
+		for (byte[] b : t) {
+			l.add(new String(b));
+		}
+		assertTrue(s.containsAll(l));
+		assertEquals(s.size(), l.size());
+
+		t.clear();
 	}
 
 	@Test
 	public void testSerializedSet() throws IOException {
-		clear();
+		SerializedSet<String> t = new SerializedSet<String>(redis, keyName);
 
-		try (Socket socket = new Socket("localhost", 6379)) {
-			SerializedSet<String> t = new SerializedSet<String>(new Redis(socket), keyName);
+		assertEquals(0, t.size());
+		t.add("Val1");
+		t.add("Val2");
+		t.add("Val2");
+		assertEquals(2, t.size());
+		assertTrue(t.contains("Val1"));
+		assertTrue(t.contains("Val2"));
+		assertTrue(!t.contains("Val3"));
+		assertTrue(t.remove("Val1"));
+		assertTrue(!t.remove("Val1"));
+		t.clear();
+		assertEquals(0, t.size());
 
-			assertEquals(0, t.size());
-			t.add("Val1");
-			t.add("Val2");
-			t.add("Val2");
-			assertEquals(2, t.size());
-			assertTrue(t.contains("Val1"));
-			assertTrue(t.contains("Val2"));
-			assertTrue(!t.contains("Val3"));
-			assertTrue(t.remove("Val1"));
-			assertTrue(!t.remove("Val1"));
-			t.clear();
-			assertEquals(0, t.size());
+		t.add("1");
+		t.add("2");
+		t.add("3");
+		t.add("4");
+		t.add("5");
 
-			t.add("1");
-			t.add("2");
-			t.add("3");
-			t.add("4");
-			t.add("5");
-
-			List<String> l = new LinkedList<>();
-			for (String b : t) {
-				l.add(b);
-			}
-			l.sort(Comparator.naturalOrder());
-
-			assertEquals("12345", String.join("", l));
-
-			t.clear();
-			assertEquals(0, t.size());
-			assertTrue(t.isEmpty());
+		List<String> l = new LinkedList<>();
+		for (String b : t) {
+			l.add(b);
 		}
+		l.sort(Comparator.naturalOrder());
 
-		clear();
+		assertEquals("12345", String.join("", l));
+
+		t.clear();
+		assertEquals(0, t.size());
+		assertTrue(t.isEmpty());
 	}
 
 	@Test
 	public void testBlockinDeque() throws IOException {
 		List<String> results = new LinkedList<>();
 		Runnable listener = () -> {
-			try (Socket s = new Socket("localhost", 6379)) {
-				Deque<String> q = new SerializedBlockingDeque<>(new Redis(s), keyName, 1);
-				results.add(q.remove());
-			} catch (IOException e) {
+			Deque<String> q = new SerializedBlockingDeque<>(redis, keyName, 1);
+			results.add(q.remove());
+		};
+
+		SerializedList<String> input = new SerializedList<>(redis, keyName);
+
+		input.add("1");
+		input.add("2");
+		input.add("3");
+		ScheduledExecutorService pool = Executors.newScheduledThreadPool(3);
+
+		List<Future<?>> futures = new LinkedList<>();
+
+		futures.add(pool.schedule(listener, 100, TimeUnit.MILLISECONDS));
+		futures.add(pool.schedule(listener, 200, TimeUnit.MILLISECONDS));
+		futures.add(pool.schedule(listener, 300, TimeUnit.MILLISECONDS));
+
+		futures.forEach(future -> {
+			try {
+				future.get();
+			} catch (InterruptedException | ExecutionException e) {
 				e.printStackTrace();
 				throw new RuntimeException(e);
 			}
-		};
+		});
 
-		clear();
-		try (Socket socket = new Socket("localhost", 6379)) {
-			SerializedList<String> input = new SerializedList<>(new Redis(socket), keyName);
+		assertTrue(results.contains("1"));
+		assertTrue(results.contains("2"));
+		assertTrue(results.contains("3"));
 
-			input.add("1");
-			input.add("2");
-			input.add("3");
-			ScheduledExecutorService pool = Executors.newScheduledThreadPool(3);
+		assertEquals(3, results.size());
+		assertEquals(0, input.size());
 
-			List<Future> futures = new LinkedList<>();
-
-			futures.add(pool.schedule(listener, 100, TimeUnit.MILLISECONDS));
-			futures.add(pool.schedule(listener, 200, TimeUnit.MILLISECONDS));
-			futures.add(pool.schedule(listener, 300, TimeUnit.MILLISECONDS));
-
-			futures.forEach(future -> {
-				try {
-					future.get();
-				} catch (InterruptedException | ExecutionException e) {
-					e.printStackTrace();
-					throw new RuntimeException(e);
-				}
-			});
-
-			assertTrue(results.contains("1"));
-			assertTrue(results.contains("2"));
-			assertTrue(results.contains("3"));
-
-			assertEquals(3, results.size());
-			assertEquals(0, input.size());
-		}
+		results.clear();
+		input.clear();
 	}
 
 	@Test
 	public void testSerializedMappedSet() throws IOException {
-		try (Socket s = new Socket("localhost", 6379)) {
-			Redis redis = new Redis(s);
+		Map<String, Set<String>> values = new SerializedMappedSet<>(Serializers.of(String.class), Serializers.of(String.class), redis, keyName);
+		Map<String, Set<String>> secondary = new SerializedMappedSet<>(Serializers.of(String.class), Serializers.of(String.class), redis, keyName);
+		values.clear();
 
-			Map<String, Set<String>> values = new SerializedMappedSet<>(Serializers.of(String.class), Serializers.of(String.class), redis, keyName);
-			Map<String, Set<String>> secondary = new SerializedMappedSet<>(Serializers.of(String.class), Serializers.of(String.class), redis, keyName);
-			values.clear();
+		assertFalse(values.get("foo").contains("bar"));
+		values.get("foo").add("bar");
+		assertTrue(values.get("foo").contains("bar"));
+		assertTrue(secondary.get("foo").contains("bar"));
+		values.get("foo").remove("bar");
+		assertEquals(1, values.size());
+		assertEquals(1, secondary.size());
+		values.clear();
+		assertEquals(0, values.size());
+		assertEquals(0, secondary.size());
+		assertFalse(values.get("foo").contains("bar"));
+		assertFalse(secondary.get("foo").contains("bar"));
 
-			assertFalse(values.get("foo").contains("bar"));
-			values.get("foo").add("bar");
-			assertTrue(values.get("foo").contains("bar"));
-			assertTrue(secondary.get("foo").contains("bar"));
-			values.get("foo").remove("bar");
-			assertEquals(1, values.size());
-			assertEquals(1, secondary.size());
-			values.clear();
-			assertEquals(0, values.size());
-			assertEquals(0, secondary.size());
-			assertFalse(values.get("foo").contains("bar"));
-			assertFalse(secondary.get("foo").contains("bar"));
+		values.get("A").add("1");
+		values.get("A").add("2");
+		values.get("A").add("3");
+		values.get("B").add("1");
+		values.get("B").add("2");
+		values.get("B").add("3");
 
-			values.get("A").add("1");
-			values.get("A").add("2");
-			values.get("A").add("3");
-			values.get("B").add("1");
-			values.get("B").add("2");
-			values.get("B").add("3");
-
-			List<String> l = new ArrayList<>();
-			for (Map.Entry<String, Set<String>> e : values.entrySet()) {
-				l.add(e.getKey() + "." + e.getValue());
-			}
-
-			assertFalse(values.isEmpty());
-
-			values.clear();
-			assertEquals(0, values.size());
-			assertTrue(values.isEmpty());
-
-			assertEquals(0, secondary.size());
-			assertTrue(secondary.isEmpty());
+		List<String> l = new ArrayList<>();
+		for (Map.Entry<String, Set<String>> e : values.entrySet()) {
+			l.add(e.getKey() + "." + e.getValue());
 		}
 
-		clear();
+		assertFalse(values.isEmpty());
+
+		values.clear();
+		assertEquals(0, values.size());
+		assertTrue(values.isEmpty());
+
+		assertEquals(0, secondary.size());
+		assertTrue(secondary.isEmpty());
 	}
 }
